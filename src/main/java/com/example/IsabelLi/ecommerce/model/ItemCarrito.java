a4e55510ec0a5ed760f49ac3f7b54f96ebbf4320
Ivan <ivannrodriguez008@gmail.com>
2026-02-02 17:49:22 -0300

    feat: itemCarrito entity

---
+++ src/main/java/com/example/IsabelLi/ecommerce/model/ItemCarrito.java
@@ -0,0 +1,88 @@
+package com.example.IsabelLi.ecommerce.model;
+import com.fasterxml.jackson.annotation.JsonIgnore;
+
+
+
+import jakarta.persistence.*;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "items_carrito")
+
+
+public class ItemCarrito {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "carrito_id", nullable = false)
+    @JsonIgnore
+    private Carrito carrito;
+
+    @ManyToOne
+    @JoinColumn(name = "producto_id", nullable = false)
+    private Producto producto;
+
+    @Column(nullable = false)
+    private Integer cantidad;
+
+    @Column(name = "precio_unitario", nullable = false, precision = 10, scale = 2)
+    private BigDecimal precioUnitario;
+
+
+    public ItemCarrito() {
+    }
+
+    public ItemCarrito(Carrito carrito, Producto producto, Integer cantidad, BigDecimal precioUnitario) {
+        this.carrito = carrito;
+        this.producto = producto;
+        this.cantidad = cantidad;
+        this.precioUnitario = precioUnitario;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Carrito getCarrito() {
+        return carrito;
+    }
+
+    public void setCarrito(Carrito carrito) {
+        this.carrito = carrito;
+    }
+
+    public Producto getProducto() {
+        return producto;
+    }
+
+    public void setProducto(Producto producto) {
+        this.producto = producto;
+    }
+
+    public Integer getCantidad() {
+        return cantidad;
+    }
+
+    public void setCantidad(Integer cantidad) {
+        this.cantidad = cantidad;
+    }
+
+    public BigDecimal getPrecioUnitario() {
+        return precioUnitario;
+    }
+
+    public void setPrecioUnitario(BigDecimal precioUnitario) {
+        this.precioUnitario = precioUnitario;
+    }
+
+    public BigDecimal getSubtotal(){
+        return precioUnitario.multiply(BigDecimal.valueOf(cantidad));
+    }
+}
