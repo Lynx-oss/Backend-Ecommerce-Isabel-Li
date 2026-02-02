d92937d909e59efa52b8ae8e5e27f7f0caa44167
Ivan <ivannrodriguez008@gmail.com>
2026-02-02 17:08:50 -0300

    feat: create CarritoRepository and findByUsuarioId method

---
+++ src/main/java/com/example/IsabelLi/ecommerce/repository/CarritoRepository.java
@@ -0,0 +1,14 @@
+package com.example.IsabelLi.ecommerce.repository;
+
+import com.example.IsabelLi.ecommerce.model.Carrito;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface CarritoRepository extends JpaRepository<Carrito, Long> {
+    Optional<Carrito> findByUsuarioId(Long usuarioId);
+}
+
+
