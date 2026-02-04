d5a52d03e57008335e547a556949dd05fde25a30
Ivan <ivannrodriguez008@gmail.com>
2026-02-04 10:05:47 -0300

    feat: add new table ImagesArray

--- src/main/java/com/example/IsabelLi/ecommerce/model/Producto.java
+++ src/main/java/com/example/IsabelLi/ecommerce/model/Producto.java
@@ -2,6 +2,8 @@ package com.example.IsabelLi.ecommerce.model;
 
 import jakarta.persistence.*;
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Producto {
@@ -12,41 +14,51 @@ public class Producto {
     private String descripcion;
     private BigDecimal precio;
     private int inventario;
-    private String imagenUrl;
+
+    @ElementCollection
+    @CollectionTable(name="producto_imagenes",
+            joinColumns = @JoinColumn(name = "producto_id"))
+    @Column(name ="imagen_url")
+    private List<String> imagenes = new ArrayList<>();
 
     @ManyToOne
     @JoinColumn(name = "categoria_id")
     private Categoria categoria;
 
-    // Constructores
     public Producto() {}
 
     public Producto(Long id, String nombre, String descripcion, BigDecimal precio,
-                    int inventario, String imagenUrl, Categoria categoria) {
+                    int inventario, List<String> imagenes ,Categoria categoria) {
         this.id = id;
         this.nombre = nombre;
         this.descripcion = descripcion;
         this.precio = precio;
         this.inventario = inventario;
-        this.imagenUrl = imagenUrl;
+        this.imagenes = imagenes;
         this.categoria = categoria;
     }
 
-    // Getters
     public Long getId() { return id; }
     public String getNombre() { return nombre; }
     public String getDescripcion() { return descripcion; }
     public BigDecimal getPrecio() { return precio; }
     public int getInventario() { return inventario; }
-    public String getImagenUrl() { return imagenUrl; }
+
+    public List<String> getImagenes() {
+        return imagenes;
+    }
+
     public Categoria getCategoria() { return categoria; }
 
-    // Setters
     public void setId(Long id) { this.id = id; }
     public void setNombre(String nombre) { this.nombre = nombre; }
     public void setDescripcion(String descripcion) { this.descripcion = descripcion; }
     public void setPrecio(BigDecimal precio) { this.precio = precio; }
     public void setInventario(int inventario) { this.inventario = inventario; }
-    public void setImagenUrl(String imagenUrl) { this.imagenUrl = imagenUrl; }
+
+    public void setImagenes(List<String> imagenes) {
+        this.imagenes = imagenes;
+    }
+
     public void setCategoria(Categoria categoria) { this.categoria = categoria; }
 }
